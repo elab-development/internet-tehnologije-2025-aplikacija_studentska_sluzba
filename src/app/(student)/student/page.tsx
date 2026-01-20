1676748d57473081a7b32eb89e5571289032b608
bijanicmilica <bijanicmilica03@gmail.com>
2026-01-21 00:57:16 +0100

    Integrisane reusable komponente na student stranici

--- src/app/(student)/student/page.tsx
+++ src/app/(student)/student/page.tsx
@@ -1,8 +1,19 @@
-export default function StudentPage(){
-    return(
-        <div className="p-8">
-            <h1 className="text-xl font-bold">Moji zahtevi</h1>
-            <p className="mt-2">Ovde će student moći da vidi svoje zahteve.</p>
-        </div>
-    );
+import StatusBadge from "@/components/StatusBadge";
+import AppButton from "@/components/AppButton";
+
+export default function StudentPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-xl font-bold">Moji zahtevi</h1>
+      
+      <div className="mt-4 border p-4 rounded">
+        <p>Uverenje o studiranju</p>
+        <StatusBadge status="Podnet" />
+      </div>
+      
+      <div className="mt-4">
+        <AppButton>Podnesi novi zahtev</AppButton>
+      </div>
+    </div>
+  );
 }
